5c67841ea00e19b119bac394d429f328417366db
ASHLEEN YOUN JI KIM <ashleenkim03@gmail.com>
2019-01-28 01:16:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,5 +9,7 @@ module.exports = {name, height, message}
 /*
 describe('Name', () => {it('returns "Susan"', () => {expect(index.name).toEqual('Susan')})})
 
+describe("Height", () => {it("is less than 40", () => {expect(index.height).toBeLessThan(40)})})
+
 
 */
