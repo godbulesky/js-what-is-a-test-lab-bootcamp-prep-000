434c731a1f829aa93beb4cdf4b66f64b47294992
ASHLEEN YOUN JI KIM <ashleenkim03@gmail.com>
2019-01-28 01:04:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ var height = 74
 var message = ` is ${height} inches tall`
 
 module.exports = {name, height, message}
+
 describe("Height", () => {it("is less than 40", () => {expect(index.height).toBeLessThan(40)})})
