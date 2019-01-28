44a7a87f1a0969e43842b699338d6b1a5448be27
ASHLEEN YOUN JI KIM <ashleenkim03@gmail.com>
2019-01-28 01:18:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
 var name = "Susan"
-var height = 74
+var height = 34
 var message = ` is ${height} inches tall`
 
 module.exports = {name, height, message}
@@ -11,5 +11,6 @@ describe('Name', () => {it('returns "Susan"', () => {expect(index.name).toEqual(
 
 describe("Height", () => {it("is less than 40", () => {expect(index.height).toBeLessThan(40)})})
 
+describe("message", () => {it("gives the name and height", () => {expect(index.message).toInclude(index.name) expect(index.message).toInclude(index.height)})})
 
 */
