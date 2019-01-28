01d785f817766eda956a178a5c4c137d518a9b3b
ASHLEEN YOUN JI KIM <ashleenkim03@gmail.com>
2019-01-28 01:10:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 // write your code below!
-var name = "Joe"
+var name = "Susan"
 var height = 74
 var message = ` is ${height} inches tall`
 
 module.exports = {name, height, message}
 
-describe("name", () =>{it('returns "Susan"',() => {expect(index.name).toEqual('Susan')})})
-describe("height", () => {it("is less than 40", () => {expect(index.height).toBeLessThan(40)})})
+
